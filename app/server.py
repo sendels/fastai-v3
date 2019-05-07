e86dc5d3a417cde0ed952a19347dae44c57e617c
Eric Sendelbach <eric.sendelbach@gmail.com>
2019-05-06 22:22:28 -0400

    Customize for my use case

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/p5hftf4qnyfm8ph/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['space_mountain', 'big_thunder_mountain', 'splash_mountain']
 path = Path(__file__).parent
 
 app = Starlette()
